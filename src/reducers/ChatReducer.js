ab171b597d2ea3c241fe2af376217202ae6fd3f2
Matheus Wallace <matheuswts2011@hotmail.com>
2019-04-03 10:25:37 -0300

    Adicionado verificações/estado no reducer

--- src/reducers/ChatReducer.js
+++ src/reducers/ChatReducer.js
@@ -1,7 +1,10 @@
 const initial_state = {
     chatList: [],
     contacts: [],
+    messages: [],
     activeChat: '',
+    activeChatOtherUserName: '',
+    message: '',
     isLoading: false
 }
 
@@ -10,14 +13,35 @@ const ChatReducer = (state = initial_state, action) => {
         case 'setContactList':
             return { ...state, contacts: action.payload.users, isLoading: action.payload.isLoading }
         break
-        case 'isLoading':
-            return { ...state, isLoading: action.payload.isLoading }
-        break
         case 'setActiveChat':
-            return { ...state, activeChat: action.payload.chatId }
+            let chatTitle = ''
+
+            for(let i in state.chatList) {
+                if(state.chatList[i].key == action.payload.chatId) {
+                    chatTitle = state.chatList[i].otherUserName
+                }
+            }
+
+            return {
+                ...state, 
+                activeChat: action.payload.chatId, 
+                activeChatOtherUserName: chatTitle
+            }
         break
         case 'setChatList':
-            return { ...state, chatList: action.payload.chats }
+            return { ...state, chatList: action.payload.chats, isLoading: action.payload.isLoading }
+        break
+        case 'setActiveChatMessages':
+            return { ...state, messages: action.payload.messages }
+        break
+        case 'handleInputChange':
+            return { ...state, message: action.payload.message }
+        break
+        case 'preSendMessage':
+            return { ...state, message: '' }
+        break
+        case 'isLoading':
+            return { ...state, isLoading: action.payload.isLoading }
         break
         case 'resetState':
             return { ...initial_state }
