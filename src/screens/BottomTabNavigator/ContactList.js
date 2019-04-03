6b1e83b7504ee557fcbe0a29df45c71b1382c314
Matheus Wallace <matheuswts2011@hotmail.com>
2019-04-03 10:28:27 -0300

    Removido componentes desnecessarios

--- src/screens/BottomTabNavigator/ContactList.js
+++ src/screens/BottomTabNavigator/ContactList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList, StyleSheet, ActivityIndicator } from 'react-native'
+import { View, FlatList, StyleSheet, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 
 import { getContactList, createChat } from '../../actions/ChatActions'
