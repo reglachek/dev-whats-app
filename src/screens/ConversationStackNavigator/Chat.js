4e59838a31070681e6bff8e30c229cd9bea7fc0f
Matheus Wallace <matheuswts2011@hotmail.com>
2019-04-03 10:29:43 -0300

    Adicionado novas funcionalidades no chat

--- src/screens/ConversationStackNavigator/Chat.js
+++ src/screens/ConversationStackNavigator/Chat.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
-import { View, Text, Platform, StyleSheet, TouchableHighlight, Image } from 'react-native'
-import { setActiveChat } from '../../actions/ChatActions'
+import { View, ImageBackground, TextInput, FlatList, StyleSheet, TouchableHighlight, Image } from 'react-native'
+import {
+    setActiveChat,
+    sendMessage,
+    handleInputChange,
+    monitorChat,
+    monitorChatOff
+} from '../../actions/ChatActions'
 import { connect } from 'react-redux'
 
-const backImage = require('../../../node_modules/react-navigation-stack/src/views/assets/back-icon.png')
+import MessageItem from '../../components/Chat/MessageItem'
 
+// Guarda imagens
+const backImage = require('../../../node_modules/react-navigation-stack/src/views/assets/back-icon.png')
+const sendImage = require('../../assets/img/send.png')
+const chatBackgroundImage = require('../../assets/img/chatBackground.png')
 
 class Chat extends Component {
     static navigationOptions = ({ navigation }) => ({
-        title: 'Olá',
+        title: navigation.getParam('otherUserName'),
+        gesturesEnabled: true,
         headerLeft: () => (
             <TouchableHighlight
                 onPress={navigation.getParam('handleBackButton')}
@@ -19,46 +30,133 @@ class Chat extends Component {
         )
     })
 
+    // User actions
     handleBackButton = async () => {
         const { setActiveChat, navigation } = this.props
 
-        await setActiveChat()
+        await setActiveChat('')
 
         navigation.goBack()
     }
 
+    handleInputChange = text => this.props.handleInputChange(text)
+
+    sendMessage = () => {
+        const { sendMessage, message, uid, activeChat } = this.props
+
+        sendMessage(message, uid, activeChat)
+    }
+
+    // FlatList
+    _keyExtractor = item => `item-key-${item.key}`
+
+    _ref = flatListReference => this.flatListArea = flatListReference
+
+    _onContentSizeChange = () => this.flatListArea.scrollToEnd({ animated:true })
+
+    _onLayout = () => this.flatListArea.scrollToEnd({ animated:true })
+
+    // Component life cycle
+    componentWillUnmount() {
+        const { monitorChatOff, activeChat } = this.props
+
+        monitorChatOff(activeChat)
+    }
+
     componentDidMount() {
-        console.log(this.handleBackButton)
-        this.props.navigation.setParams({
+        const { navigation, monitorChat, activeChat } = this.props
+
+        navigation.setParams({
             handleBackButton: this.handleBackButton
         })
+
+        monitorChat(activeChat)
     }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Chat</Text>
-            </View>
+            <ImageBackground
+                source={chatBackgroundImage}
+                style={styles.container}
+            >
+                <FlatList 
+                    data={this.props.messages}
+                    renderItem={({ item }) => <MessageItem
+                        data={item}
+                        authUserUid={this.props.uid}
+                    />}
+                    keyExtractor={this._keyExtractor}
+                    ref={this._ref}
+                    onContentSizeChange={this._onContentSizeChange}
+                    onLayout={this._onLayout}
+                />
+
+                <View style={styles.sendArea}>
+                    <TextInput
+                        style={styles.sendInput}
+                        value={this.props.message}
+                        onChangeText={this.handleInputChange} 
+                    />
+
+                    <TouchableHighlight
+                        style={styles.sendButton}
+                        underlayColor='transparent'
+                        onPress={this.sendMessage}
+                    >
+                        <Image source={sendImage} style={styles.sendImage} />
+                    </TouchableHighlight>
+                </View>
+            </ImageBackground>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: Platform.OS === 'ios' ? 20 : 0,
-    },
     imageBack: {
         width: 25,
         height: 25,
         marginLeft: 20,
+    },
+    container: {
+        width: '100%', 
+        height: '100%'
+    },
+    sendArea: {
+        flexDirection: 'row',
+        height: 50,
+        backgroundColor: '#EEEEEE'
+    },
+    sendInput: {
+        flex: 1,
+        height: 50,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+    },
+    sendButton: {
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    sendImage: {
+        height: 40,
+        width: 40,
     }
 })
 
 const mapStateToProps = state => ({
     uid: state.AuthReducer.uid,
+    activeChat: state.ChatReducer.activeChat,
+    message: state.ChatReducer.message,
+    messages: state.ChatReducer.messages
 })
 
-const ConnectChat = connect(mapStateToProps, { setActiveChat })(Chat)
+const ConnectChat = connect(mapStateToProps, {
+    setActiveChat,
+    sendMessage,
+    handleInputChange,
+    monitorChat,
+    monitorChatOff
+})(Chat)
 
 export default ConnectChat
